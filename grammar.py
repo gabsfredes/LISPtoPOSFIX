1d2a90e45739b7e5c79bc76abc039d2c119e79a7
Gabriel Fredes <gabriel.sfredes@gmail.com>
2024-06-23 19:25:37 -0300

    updt: Regras de gramática
    Atualizado de operações matemáticas para conversão em pós fixação

--- grammar.py
+++ grammar.py
@@ -4,39 +4,39 @@ from trabalho import tokens
 # Definição da gramática (regras de produção)
 def p_expressao_mais(p):
     "expressao : expressao MAIS termo"
-    p[0] = p[1] + p[3]
+    p[0] = f"{p[1]} {p[3]} +"
 
 def p_expressao_menos(p):
     "expressao : expressao MENOS termo"
-    p[0] = p[1] - p[3]
+    p[0] = f"{p[1]} {p[3]} -"
 
 def p_expressao_termo(p):
     "expressao : termo"
-    p[0] = p[1]
+    p[0] = str(p[1])
 
 def p_termo_vezes(p):
     "termo : termo VEZES fator"
-    p[0] = p[1] * p[3]
+    p[0] = f"{p[1]} {p[3]} *"
     
 def p_termo_dividir(p):
     "termo : termo DIVIDIR fator"
-    p[0] = p[1] / p[3]
+    p[0] = f"{p[1]} {p[3]} /"
 
 def p_termo_fator(p):
     "termo : fator"
-    p[0] = p[1]
+    p[0] = str(p[1])
     
 def p_fator_numero(p):
     "fator : NUMERO"
-    p[0] = p[1]
+    p[0] = str(p[1])
 
 def p_fator_id(p):
     "fator : ID"
-    p[0] = p[1]
+    p[0] = str(p[1])
 
 def p_fator_parenteses(p):
     "fator : ABRE_PAREN expressao FECHA_PAREN"
-    p[0] = p[2]
+    p[0] = str(p[2])
     
 def p_error(p):
     print("Erro de sintaxe!")
@@ -48,4 +48,3 @@ parser = yacc.yacc()
 entrada = "((2 + 3) * 4) / 2" 
 resultado = parser.parse(entrada)
 print("Resultado: %s" % resultado) 
-
